5f8efae092ff03f3acf8e0922d86c968653c9657
tejuypatil <tejuypatil@gmail.com>
2022-06-23 15:44:22 +0530

    Added daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -14,3 +14,12 @@ else
 	echo "Emplyoee is absent"
 fi
 
+
+echo "------------UC2-----------"
+EmpWagePerHour=20;
+FullDayHour=8;
+
+salary=$(( $EmpWagePerHour * $FullDayHour ));
+echo "Employee Wage is $salary for one day"
+
+
